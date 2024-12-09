6357335e9deeb533220d3cb8c3ce622909240acd
SruthiPrasannakumar <sruthipras93@gmail.com>
2024-12-09 12:41:23 +0530

    second commit

---
+++ controllers/projectController.js
@@ -0,0 +1,89 @@
+const projects=require('../models/projectModel')
+
+// add project
+exports.addProjectController=async(req,res)=>{
+    console.log("Inside addProjectController");
+    const userId=req.userId
+    
+    
+    // console.log(userId);
+    // console.log(req.body);
+    // console.log(req.file);
+    const {title,languages,overview,github,website}=req.body
+    const projectImage=req.file.filename
+    try{
+        const existingProject=await projects.findOne({github}) 
+        if(existingProject){
+            res.status(406).json("Project Already exists...Please upload another!!!")
+
+        }else{
+            const newProject=new projects({
+                title,languages,overview,github,website,projectImage,userId
+            })
+            await newProject.save()
+            res.status(200).json(newProject)
+
+        }
+
+    }catch(err){
+        res.status(401).json(err)
+
+    }
+    
+    
+   
+    
+}
+
+// get home projects-guest user
+exports.getHomeProjectsController=async(req,res)=>{
+    console.log("Inside getHomeProjectsController");
+    try{
+        const allHomeProjects=await projects.find().limit(3)
+        res.status(200).json(allHomeProjects)
+
+    }catch(err){
+        res.status(401).json(err)
+
+    }
+    
+}
+
+
+// get user projects-authorised user
+exports.getUserProjectsController=async(req,res)=>{
+    console.log("Inside getUserProjectsController");
+    const userId=req.userId
+    try{
+        const allUserProjects=await projects.find({userId})
+        res.status(200).json(allUserProjects)
+
+    }catch(err){
+        res.status(401).json(err)
+
+    }
+    
+}
+
+
+// get all projects-authorised user
+exports.getAllProjectsController=async(req,res)=>{
+    console.log("Inside getAllProjectsController");
+    // to get query parameter from url use req.query
+    const searchKey=req.query.search
+    
+    try{
+        // to get documents from model which matches search query in languages
+        const allProjects=await projects.find({
+            languages:{
+                $regex:searchKey,$options:"i"
+            }
+        })
+        res.status(200).json(allProjects)
+
+    }catch(err){
+        res.status(401).json(err)
+
+    }
+    
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,65 @@
-
+const users=require('../models/userModel')
+const jwt=require('jsonwebtoken')
 // register
-exports.registerController=(req,res)=>{
+exports.registerController= async (req,res)=>{
     console.log("Inside registerController");
     const {username,email,password}=req.body
     console.log(username,email,password);
+
+    try{
+        const exisitingUser=await users.findOne({email})
+        if(exisitingUser){
+            res.status(406).json("User Already exist...Please Login!!!")
+
+        }else{
+            const newUser=new users({
+                username,email,password,github:"",linkedin:"",profilePic:""
+            })
+            await newUser.save()
+            res.status(200).json(newUser)
+
+        }
+
+    }catch(err){
+        res.status(401).json(err)
+
+    }
+
     
-    res.status(200).json("Request recieved!!!")
+   
     
 }
 
 // login
+exports.loginController= async (req,res)=>{
+    console.log("Inside loginController");
+    const {email,password}=req.body
+    console.log(email,password);
+
+    try{
+        const exisitingUser=await users.findOne({email,password})
+        if(exisitingUser){
+            // token generate
+            const token=jwt.sign({userId:exisitingUser._id},process.env.JWTPASSWORD)
+            res.status(200).json({
+                user:exisitingUser,
+                token
+            })
+
+        }else{
+           
+            res.status(404).json("Invalid Email/Password")
+
+        }
+
+    }catch(err){
+        res.status(401).json(err)
+
+    }
+
+    
+   
+    
+}
 
 // profile updation
